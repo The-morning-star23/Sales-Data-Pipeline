edf6da1c6e7dc38a847abd3890a13bfaf2d7fc6a
Shubh Kumar <kumarshubh263@gmail.com>
2025-01-02 00:24:23 +0530

    commit

--- dashboards/app.py
+++ dashboards/app.py
@@ -58,4 +58,4 @@ def api_sales():
         return {"error": "Unable to load sales data."}, 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
